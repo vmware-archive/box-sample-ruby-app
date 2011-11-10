bc5d5b5ee2190e093178c6a99d0bb4ed9e9b4f71
Luke Curley <luke@box.net>
2011-11-09 19:31:08 -0800

    Added arrow to the last selected item.
    This also involved removing the spriting mechanism.

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,3 +1,24 @@
+$(document).ready(function() {
+  $('li.item').live('click', function() {
+    var id = $(this).data('id');
+    var type = $(this).data('type');
+    var column = $(this).parents('td');
+
+    $('li.item.selected', column).removeClass('selected');
+    $(this).addClass('selected');
+
+    remove_next_columns(column);
+    spinny = add_next_column(column, "<div class=\"spinny\"></div>");
+
+    var action = (type == 'folder') ? 'items' : 'preview';
+
+    $.get(action + '/' + id, function(data) {
+      spinny.remove();
+      add_next_column(column, data);
+    });
+  });
+});
+
 function add_next_column(column, data, speed) {
   speed = speed || 'fast';
   data = "<td class=\"pane\">" + data + "</td>";
@@ -12,21 +33,3 @@ function remove_next_columns(column, speed) {
     $(this).remove;
   });
 }
-
-$(document).ready(function() {
-  $('li.item').live('click', function() {
-    var id = $(this).data('id');
-    var type = $(this).data('type');
-    var column = $(this).parents('td');
-
-    remove_next_columns(column);
-    spinny = add_next_column(column, "<div class=\"spinny\"></div>");
-
-    var action = (type == 'folder') ? 'items' : 'preview';
-
-    $.get(action + '/' + id, function(data) {
-      spinny.remove();
-      add_next_column(column, data);
-    });
-  });
-});
